9b9d6b1bc24208e786106e2ee1e4153dda749de2
shivakumarreddy <b.skreddy1997@gmail.com>
2024-10-28 17:57:45 -0400

    Next.js config for GitHub pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/portfolio",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
